8933d971d071f58aa473d169521faf54707bd59b
GeekCornerGH <45696571+GeekCornerGH@users.noreply.github.com>
2025-09-25 15:01:01 +0200

    refactor (Sponsors): Move Sponsors box to a component

---
+++ src/components/GitHubSponsorsBox.tsx
@@ -0,0 +1,3 @@
+export default function GitHubSponsorsBox() {
+    return <iframe src="https://github.com/sponsors/EarthMaker-MC/card" title="Sponsor EarthMaker-MC" height="90" width="600" style={{border: 0}}></iframe>  
+}
