673fbaf722d2564679db3cf3768b8754d1042d4a
GeekCornerGH <45696571+GeekCornerGH@users.noreply.github.com>
2026-01-10 11:30:36 +0100

    feat (announcementBar): Add announcement for the Crans Montana tragedy

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -83,6 +83,11 @@ const config: Config = {
       insights: true,
       askAi: "GOGA9mswVXY3"
     },
+    announcementBar: {
+      id: "2026-crans-montana",
+      content: "After this tragic accident, the EarthMaker team stands with the Crans-Montana victims and their families 🎗️🇨🇭",
+      isCloseable: true
+    },
     zoom: {
       selector: '.markdown img',
       config: {
